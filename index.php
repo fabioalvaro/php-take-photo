b8319be99f0193f9a56e8bc7e7e9097b0e375512
fabioalvaro <fabio.alvaro@gmail.com>
2016-02-17 07:43:06 -0200

    initial post

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Tirar Fotos</title>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<style>
+           video { border: 1px solid #ccc; display: block; margin: 0 0 20px 0; }
+        #canvas { margin-top: 20px; border: 1px solid #ccc; display: block; }
+</style>
+
+
+</head>
+<body>
+<div>
+    <div><video id="video" width="640" height="480" autoplay></video></div>
+    <div><button id="snap">Tirar Foto</button></div>
+    <div><button id="save">Salvar Foto</button></div>
+    <div><canvas id="canvas" width="640" height="480"></canvas></div>
+<script>
+
+</script>    
+<script src="foto.js" type="text/javascript"></script>
+</body>
+</html>
